113582db459268cedbc46a45582d93a8769e5e2e
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-05-20 21:53:57 +0530

    Time: 231 ms (18.04%), Space: 63.3 MB (9.01%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 1) return 0;
+        int[] notPrime = new int[n];
+        notPrime[0] = notPrime[1] = 1;
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == 0) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = 1;
+                }
+            }
+        }
+        return count;
+    }
+}
