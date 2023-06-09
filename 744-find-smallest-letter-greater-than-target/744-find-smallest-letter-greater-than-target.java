cadeaee5ed8f7ec6a43ef56f6edbb4ad8a91ffc3
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-06-09 10:17:59 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (23.02%) - LeetHub

---
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,6 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (char letter : letters) if (letter > target) return letter;
+        return letters[0];
+    }
+}
