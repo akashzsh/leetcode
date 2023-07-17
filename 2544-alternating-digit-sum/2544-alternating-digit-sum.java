db5b49db90077dbcf95c5178347b56e8d38279b8
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-07-17 21:29:13 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (37.29%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum = -sum + n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
