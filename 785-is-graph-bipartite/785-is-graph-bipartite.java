f03fdabe48368b456ebf391094ea9b6cae808f4a
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-07-18 19:55:29 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (53.14%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,34 +1,21 @@
 class Solution {
-    public boolean isBipartite(int node, int[] color, ArrayList<ArrayList<Integer>> adj) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        color[node] = 1;
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            for (int i : adj.get(curr)) {
-                if (color[i] == -1) {
-                    color[i] = 1 - color[curr];
-                    q.offer(i);
-                } else if (color[i] == color[curr]) return false;
-            }
+    public boolean dfs(int node, int c, int[][] graph, int[] color) {
+        color[node] = c;
+        for (int i = 0; i < graph[node].length; i++) {
+            if (color[graph[node][i]] == -1) {
+                if (!dfs(graph[node][i], 1 - c, graph, color)) return false;
+            } else if (color[graph[node][i]] == color[node]) return false;
         }
         return true;
     }
     
     public boolean isBipartite(int[][] graph) {
         int len = graph.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < len; i++) adj.add(new ArrayList<>());
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                adj.get(i).add(graph[i][j]);
-            }
-        }
         int[] color = new int[len];
         for (int i = 0; i < len; i++) color[i] = -1;
         for (int i = 0; i < len; i++) {
             if (color[i] == -1) {
-                if (!isBipartite(i, color, adj)) return false;
+                if (!dfs(i, 0, graph, color)) return false;
             }
         }
         return true;
