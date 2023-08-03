c0e8d58198f2b8fb3816a5344ea77c410e160a8b
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-08-03 21:21:05 +0530

    Time: 33 ms (63.25%), Space: 43.4 MB (97.19%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[][][] dp = new int[n][m][m];
+        int[][] prev = new int[m][m];
+        int[][] curr = new int[m][m];
         for (int j1 = 0; j1 < m; j1++) {
             for (int j2 = 0; j2 < m; j2++) {
-                if (j1 == j2) dp[n - 1][j1][j2] = grid[n - 1][j1];
-                else dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                if (j1 == j2) prev[j1][j2] = grid[n - 1][j1];
+                else prev[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
         for (int i = n - 2; i >= 0; i--) {
@@ -18,15 +19,18 @@ class Solution {
                             if (j1 == j2) val = grid[i][j1];
                             else val = grid[i][j1] + grid[i][j2];
                             if (j1 + dj1 >= 0 && j2 + dj2 >= 0 && j1 + dj1 < m && j2 + dj2 < m)
-                            val += dp[i + 1][j1 + dj1][j2 + dj2];
+                            val += prev[j1 + dj1][j2 + dj2];
                             else val += (int)(-1e8);
                             maxi = Math.max(maxi, val);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            for (int a = 0; a < m; a++) {
+                prev[a] = (int[])(curr[a].clone());
+            }
         }
-        return dp[0][0][m -1];
+        return prev[0][m -1];
     }
 }
