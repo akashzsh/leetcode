e7a8762ed9504bade3e6f4af781376cfce94247e
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-08-10 23:38:32 +0530

    Time: 15 ms (86.33%), Space: 43.6 MB (45.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int len = coins.length;
-        int[][] dp = new int[len][amount + 1];
+        int[] prev = new int[amount + 1];
         for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0) dp[0][i] = i / coins[0];
-            else dp[0][i] = (int)(1e9);
+            if (i % coins[0] == 0) prev[i] = i / coins[0];
+            else prev[i] = (int)(1e9);
         }
         for (int index = 1; index < len; index++) {
+            int[] curr = new int[amount + 1];
             for (int target = 0; target <= amount; target++) {
-                int notPick = 0 + dp[index - 1][target];
+                int notPick = 0 + prev[target];
                 int pick = (int)(1e9);
                 if (coins[index] <= target)
-                    pick = 1 + dp[index][target - coins[index]];
-                dp[index][target] = Math.min(pick, notPick);
+                    pick = 1 + curr[target - coins[index]];
+                curr[target] = Math.min(pick, notPick);
             }
+            prev = curr;
         }
-        int ans = dp[len - 1][amount];
+        int ans = prev[amount];
         return ans >= (int)(1e9) ? -1 : ans;
     }
 }
