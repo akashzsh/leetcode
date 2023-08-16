6dfd4bfda853fc0977a9d8bcd1eca1f80ae3b274
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-08-16 19:31:27 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String[] S = s1.split(" ");
+            String s = S[0];
+            String t = S[1];
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s, t);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= m; i++) dp[0][i] = i;
+        for (int i = 0; i <= n; i++) dp[i][0] = i;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = 1 + dp[i][j - 1];
+                    int replace = 1 + dp[i - 1][j - 1];
+                    int delete = 1 + dp[i - 1][j];
+                    dp[i][j] = Math.min(insert, Math.min(replace, delete));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
