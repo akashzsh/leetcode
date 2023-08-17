f10c2d545ceab6a6bf45315ef64af7c8a8ce3d33
Akash <80875594+akashinfinity12@users.noreply.github.com>
2023-08-17 18:15:24 +0530

    Added solution - LeetHub

---
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine().trim());
+        while(t-- > 0){
+            
+            int n; 
+            n = Integer.parseInt(br.readLine().trim());
+            
+            
+            int[] price = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.maxProfit(n, price);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int maxProfit(int n, int[] prices) {
+        int[][] prev = new int[2][3];
+        for (int day = n - 1; day >= 0; day--) {
+            int[][] curr = new int[2][3];
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy == 1) {
+                        int get = -prices[day] + prev[0][cap];
+                        int notGet = 0 + prev[1][cap];
+                        curr[buy][cap] = Math.max(get, notGet);
+                    } else {
+                        int drop = prices[day] + prev[1][cap - 1];
+                        int notDrop = 0 + prev[0][cap];
+                        curr[buy][cap] = Math.max(drop, notDrop);
+                    }
+                }
+            }
+            prev = curr;
+        }
+        return prev[1][2];
+    }
+}
