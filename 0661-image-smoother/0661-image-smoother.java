3d769869e4b55f40ed845ade85a33194c39970d3
Akash <80875594+akashzsh@users.noreply.github.com>
2023-12-19 21:02:03 +0530

    Time: 8 ms (23.81%), Space: 44.6 MB (57.42%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int den = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (x >= 0 && x < m && y >= 0 && y < n) {
+                            res[i][j] += img[x][y];
+                            den++;
+                        }
+                    }
+                }
+                res[i][j] /= den;
+            }
+        }
+        return res;
+    }
+}
