6caa30e61c944511774c96c1053c9680d14c64d2
Akash <80875594+akashzsh@users.noreply.github.com>
2023-12-25 16:44:42 +0530

    Time: 1 ms (84.19%), Space: 41.3 MB (18.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,31 +1,23 @@
 class Solution {
-    private int f(int i, String s, int n, int[] dp) {
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        
-        if (i == n) {
-            return dp[i] = 1;
-        }
-        
-        if (s.charAt(i) == '0') {
-            return dp[i] = 0;
-        }
-        
-        int result = f(i + 1, s, n, dp);
-        if (i + 1 < n) {
-            if (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) <= '6')) {
-                result += f(i + 2, s, n, dp);
-            }
-        }
-        
-        return dp[i] = result;
-    }
-    
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return f(0, s, n, dp);
+        
+        dp[n] = 1;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i + 1 < n) {
+                    if (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) <= '6')) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            }
+        }
+        
+        return dp[0];
     }
 }
