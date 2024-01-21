21cc14591b2acddc9d7c5518208b4b282e4ceb50
Akash <80875594+akashzsh@users.noreply.github.com>
2024-01-21 12:13:46 +0530

    Time: 12 ms (92.71%), Space: 58.3 MB (35.31%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,7 +1,5 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        //   0 1 2 3 4 5 6 7 8 9
-        //  [2,2,2,3,3,4,4,4,4,4]
         int n = tasks.length;
         Arrays.sort(tasks);
         int cnt = 0;
