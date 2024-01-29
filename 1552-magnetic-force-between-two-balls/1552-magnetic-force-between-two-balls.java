99d5922943e9c70ba950e682f48e46c0723a64ae
Akash <80875594+akashzsh@users.noreply.github.com>
2024-01-29 20:19:28 +0530

    Time: 41 ms (89.81%), Space: 55.7 MB (67.53%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,14 +1,13 @@
 class Solution {
-    private boolean isPossible(int[] position, int mid, int m) {
-        int cnt = 1, last = position[0];
+    public boolean isPossible(int[] position, int dist, int m) {
+        int curBalls = 1, last = position[0];
         for (int i = 1; i < position.length; i++) {
-            if (position[i] - last >= mid) {
-                cnt++;
+            if (position[i] - last >= dist) {
+                curBalls++;
                 last = position[i];
             }
-            if (cnt >= m) return true;
         }
-        return false;
+        return curBalls >= m;
     }
     
     public int maxDistance(int[] position, int m) {
@@ -17,8 +16,11 @@ class Solution {
         int low = 1, high = position[n - 1] - position[0];
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if (isPossible(position, mid, m)) low = mid + 1;
-            else high = mid - 1;
+            if (isPossible(position, mid, m)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
         return high;
     }
