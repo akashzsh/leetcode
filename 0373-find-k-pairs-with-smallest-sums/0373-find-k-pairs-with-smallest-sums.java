27dfea4334216554bcc62410383797bef0289656
Akash <80875594+akashzsh@users.noreply.github.com>
2024-01-31 21:34:19 +0530

    Time: 57 ms (50.15%), Space: 60.3 MB (45.93%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int n = nums1.length, m = nums2.length;
+        int len1 = nums1.length, len2 = nums2.length;
         
         List<List<Integer>> res = new ArrayList<>();
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        HashSet<Pair<Integer, Integer>> vis = new HashSet<>();
+        Set<Pair<Integer, Integer>> set = new HashSet<>();
         
-        pq.offer(new int[]{nums1[0] + nums2[0], 0, 0});
-        vis.add(new Pair<Integer, Integer>(0, 0));
+        pq.add(new int[]{nums1[0] + nums2[0], 0, 0});
+        set.add(new Pair(0, 0));
         
         while (k > 0 && !pq.isEmpty()) {
             int[] cur = pq.poll();
@@ -15,14 +15,14 @@ class Solution {
             int j = cur[2];
             res.add(List.of(nums1[i], nums2[j]));
             
-            if (i + 1 < n && !vis.contains(new Pair<Integer, Integer>(i + 1, j))) {
-                pq.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
-                vis.add(new Pair<Integer, Integer>(i + 1, j));
+            if (j + 1 < len2 && !set.contains(new Pair(i, j + 1))) {
+                pq.add(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
+                set.add(new Pair(i, j + 1));
             }
             
-            if (j + 1 < m && !vis.contains(new Pair<Integer, Integer>(i, j + 1))) {
-                pq.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
-                vis.add(new Pair<Integer, Integer>(i, j + 1));
+            if (i + 1 < len1 && !set.contains(new Pair(i + 1, j))) {
+                pq.add(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
+                set.add(new Pair(i + 1, j));
             }
             
             k--;
