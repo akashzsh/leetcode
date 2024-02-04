ed7db19840e932b4cb684c25bf25e9316c37955e
Akash <80875594+akashzsh@users.noreply.github.com>
2024-02-04 17:23:11 +0530

    Time: 7 ms (62.87%), Space: 44 MB (86.33%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,49 +1,30 @@
-class Pair {
-    String word;
-    int freq;
-    
-    Pair(String word, int freq) {
-        this.word = word;
-        this.freq = freq;
-    }
-}
-
-class CustomComparator implements Comparator<Pair> {
+class CustomComparator implements Comparator<Map.Entry<String, Integer>> {
     @Override
-    public int compare(Pair p1, Pair p2) {
-        if (p1.freq != p2.freq) {
-            return p1.freq - p2.freq;
-        }
-        String word1 = p1.word;
-        String word2 = p2.word;
-        return word2.compareTo(word1);
+    public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+        if (e1.getValue() < e2.getValue()) return -1;
+        if (e1.getValue() > e2.getValue()) return 1;
+        if (e1.getKey().compareTo(e2.getKey()) < 0) return 1;
+        return -1;
     }
 }
 
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        int n = words.length;
         Map<String, Integer> freq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < words.length; i++) {
             freq.put(words[i], freq.getOrDefault(words[i], 0) + 1);
         }
         CustomComparator c = new CustomComparator();
-        PriorityQueue<Pair> pq = new PriorityQueue<>(c);
-        for (Map.Entry<String, Integer> cur : freq.entrySet()) {
-            Pair p1 = new Pair(cur.getKey(), cur.getValue());
-            if (pq.size() >= k) {
-                if (c.compare(p1, pq.peek()) > 0) {
-                    pq.poll();
-                } else {
-                    continue;
-                }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(c);
+        for (Map.Entry<String, Integer> e : freq.entrySet()) {
+            pq.add(e);
+            if (pq.size() > k) {
+                pq.poll();
             }
-            pq.add(p1);
         }
-        List<String> res = new ArrayList<>();
-        n = pq.size();
-        for (int i = 0; i < n; i++) {
-            res.add(0, pq.poll().word);
+        List<String> res = new LinkedList<>();
+        while (!pq.isEmpty()) {
+            res.add(0, pq.poll().getKey());
         }
         return res;
     }
