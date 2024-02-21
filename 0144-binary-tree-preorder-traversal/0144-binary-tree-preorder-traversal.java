349939ac63f7eb2b845483e1857196d08f4a5bbd
Akash <80875594+akashzsh@users.noreply.github.com>
2024-02-21 21:42:03 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (32.95%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,3 +1,37 @@
+// Morris Traversal
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = root;
+        
+        while (cur != null) {
+            if (cur.left == null) {
+                res.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+                if (prev.right == null) {
+                    prev.right = cur;
+                    res.add(cur.val);
+                    cur = cur.left;
+                } else {
+                    prev.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
+
+// Iterative
+
+/*
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -23,7 +57,7 @@ class Solution {
         return res;
     }
 }
-
+*/
 
 // Recursion
 
