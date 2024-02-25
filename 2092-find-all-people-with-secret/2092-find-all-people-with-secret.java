313c5734c11374bc9a10584d97186a0cfe786c5f
Akash <80875594+akashzsh@users.noreply.github.com>
2024-02-25 12:57:55 +0530

    Time: 290 ms (14.00%), Space: 125.8 MB (6.00%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,36 +1,80 @@
+class DisjointSet {
+    int[] rank;
+    int[] parent;
+    
+    DisjointSet(int n) {
+        rank = new int[n];
+        parent = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int findUParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findUParent(parent[node]);
+    }
+    
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u), ulp_v = findUParent(v);
+        if (ulp_u == ulp_v) {
+            return;
+        }
+        if (rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        } else if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+    void reset(int node) {
+        parent[node] = node;
+        rank[node] = 0;
+    }
+}
+
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        HashMap<Integer, ArrayList<int[]>> graph = new HashMap<>();
-        for (int[] meeting : meetings) {
-            int x = meeting[0], y = meeting[1], t = meeting[2];
-            graph.computeIfAbsent(x, k -> new ArrayList<>()).add(new int[]{t, y});
-            graph.computeIfAbsent(y, k -> new ArrayList<>()).add(new int[]{t, x});
-        }
+        Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
+        DisjointSet ds = new DisjointSet(n);
         
-        int[] earliest = new int[n];
-        Arrays.fill(earliest, Integer.MAX_VALUE);
-        earliest[0] = 0;
-        earliest[firstPerson] = 0;
-
-        dfs(0, 0, graph, earliest);
-        dfs(firstPerson, 0, graph, earliest);
+        ds.unionByRank(0, firstPerson);
         
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            if (earliest[i] != Integer.MAX_VALUE) {
-                ans.add(i);
+        int i = 0;
+        while (i < meetings.length) {
+            int curTime = meetings[i][2];
+            ArrayList<Integer> sameTimePeople = new ArrayList<>();
+            
+            while (i < meetings.length && meetings[i][2] == curTime) {
+                ds.unionByRank(meetings[i][0], meetings[i][1]);
+                sameTimePeople.add(meetings[i][0]);
+                sameTimePeople.add(meetings[i][1]);
+                i++;
+            }
+            
+            System.out.println(sameTimePeople);
+            
+            for (int j : sameTimePeople) {
+                if (ds.findUParent(j) != ds.findUParent(0)) {
+                    ds.reset(j);
+                }
             }
         }
-        return ans;
-    }
-
-    private void dfs(int person, int time, HashMap<Integer, ArrayList<int[]>> graph, int[] earliest) {
-        for (int[] nextPersonTime : graph.getOrDefault(person, new ArrayList<>())) {
-            int t = nextPersonTime[0], nextPerson = nextPersonTime[1];
-            if (t >= time && earliest[nextPerson] > t) {
-                earliest[nextPerson] = t;
-                dfs(nextPerson, t, graph, earliest);
+        
+        List<Integer> res = new ArrayList<>();
+        
+        for (i = 0; i < n; i++) {
+            if (ds.findUParent(i) == ds.findUParent(0)) {
+                res.add(i);
             }
         }
+        
+        return res;
     }
 }
