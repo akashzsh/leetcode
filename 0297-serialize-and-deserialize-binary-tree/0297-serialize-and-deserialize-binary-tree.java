3fc92e1ffe0a02e1c78c45d756eb74e2be4ceb94
Akash <80875594+akashzsh@users.noreply.github.com>
2024-03-08 22:24:16 +0530

    Time: 20 ms (31.37%), Space: 45.6 MB (73.05%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,57 @@
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            
+            if (cur != null) {
+                sb.append(cur.val + ",");
+            } else {
+                sb.append("n" + ",");
+                continue;
+            }
+            
+            q.add(cur.left);
+            q.add(cur.right);
+        }
+        
+        return sb.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.equals("")) {
+            return null;
+        }
+        
+        String[] values = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root);
+        
+        for (int i = 1; i < values.length; i++) {
+            TreeNode parent = q.poll();
+            
+            if (!values[i].equals("n")) {
+                parent.left = new TreeNode(Integer.parseInt(values[i]));
+                q.add(parent.left);
+            }
+            
+            if (!values[++i].equals("n")) {
+                parent.right = new TreeNode(Integer.parseInt(values[i]));
+                q.add(parent.right);
+            }
+        }
+        
+        return root;
+    }
+}
