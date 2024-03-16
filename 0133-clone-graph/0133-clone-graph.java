6c3e3d8b6aff9c19dcd0322ac140dc89d7cfcd44
Akash <80875594+akashzsh@users.noreply.github.com>
2024-03-16 12:42:20 +0530

    Time: 26 ms (59.11%), Space: 42.6 MB (53.77%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,22 +1,32 @@
 class Solution {
-    public void f(Node node, HashMap<Node, Node> map) {
+    
+    public void dfs(Node node, HashMap<Node, Node> map) {
+        
         Node newNode = new Node(node.val);
         map.put(node, newNode);
         
         for (Node neighbor : node.neighbors) {
+            
             if (!map.containsKey(neighbor)) {
-                f(neighbor, map);
+                dfs(neighbor, map);
             }
+            
             newNode.neighbors.add(map.get(neighbor));
+            
         }
+        
     }
     
     public Node cloneGraph(Node node) {
+        
         if (node == null) {
             return null;
-        }        
+        }
+        
         HashMap<Node, Node> map = new HashMap<>();
-        f(node, map);
+        
+        dfs(node, map);
+        
         return map.get(node);
     }
 }
