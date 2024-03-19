fff1d6065d34aa4a215c2d0584216b02124178e9
Akash <80875594+akashzsh@users.noreply.github.com>
2024-03-19 21:25:54 +0530

    Time: 2 ms (99.12%), Space: 45.7 MB (56.73%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        Arrays.sort(freq);
+        
+        int maxFreq = freq[25] - 1;
+        
+        int idleSlots = maxFreq * n;
+
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+            idleSlots -= Math.min(maxFreq, freq[i]);
+        }
+
+        return idleSlots <= 0 ? tasks.length : idleSlots + tasks.length;
+    }
+}
