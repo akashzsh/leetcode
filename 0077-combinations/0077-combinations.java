50cd180eff1460fb14dcbf397c7faaa04f89d163
Akash <80875594+akashzsh@users.noreply.github.com>
2024-04-18 22:53:35 +0530

    Time: 28 ms (24.35%), Space: 94.5 MB (52.71%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,27 +1,19 @@
 class Solution {
-    public void f(int i, int[] nums, List<List<Integer>> res, List<Integer> ds, int k) {
-        if (i >= nums.length) {
+    public void f(int i, List<List<Integer>> res, List<Integer> ds, int k, int n) {
+        if (i >= n) {
             if (ds.size() == k) res.add(new ArrayList<>(ds));
             return;
         }
         
-        ds.add(nums[i]);
-        f(i + 1, nums, res, ds, k);
+        ds.add(i + 1);
+        f(i + 1, res, ds, k, n);
         ds.remove(ds.size() - 1);
-        f(i + 1, nums, res, ds, k);
+        f(i + 1, res, ds, k, n);
     }
     
     public List<List<Integer>> combine(int n, int k) {
-        int[] nums = new int[n];
-        
-        for (int i = 1; i <= n; i++) {
-            nums[i - 1] = i;
-        }
-        
         List<List<Integer>> res = new ArrayList<>();
-        
-        f(0, nums, res, new ArrayList<>(), k);
-        
+        f(0, res, new ArrayList<>(), k, n);       
         return res;
     }
 }
