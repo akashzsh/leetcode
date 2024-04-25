be9d975cd26e27601fe6739da6a8b8a4563a0bf4
Akash <80875594+akashzsh@users.noreply.github.com>
2024-04-25 22:45:48 +0530

    Time: 3 ms (66.98%), Space: 45.5 MB (26.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,35 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
-        for (int i = 0; i < nums.length; i++) {
-            ones = (ones ^ nums[i]) & ~twos;
-            twos = (twos ^ nums[i]) & ~ones;
-        }
-        return ones;
-    }
-}
-
-
-/*
-Approach 2 (Intuitive)
-
-class Solution {
-    public int singleNumber(int[] nums) {
-        int sum = 0, res = 0;
-        for (int i = 0; i < 32; i++) {
-            sum = 0;
+        int res = 0;
+        
+        for (int i = 0, cnt = 0; i < 32; i++, cnt = 0) {
             for (int j = 0; j < nums.length; j++) {
-                if (((nums[j] >> i) & 1) == 1) {
-                    sum++;
+                if ((nums[j] & (1 << i)) != 0) {
+                    cnt++;
                 }
             }
-            sum = sum % 3;
-            if (sum != 0) {
-                res = res | (sum << i);
+            if (cnt % 3 != 0) {
+                res = res | (1 << i);
             }
         }
+        
         return res;
     }
 }
-
-*/
