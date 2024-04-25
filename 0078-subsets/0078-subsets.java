645a9210be005a37225a420dcae0d4176449d6c2
Akash <80875594+akashzsh@users.noreply.github.com>
2024-04-25 20:42:23 +0530

    Time: 1 ms (60.46%), Space: 42.6 MB (60.24%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,10 +1,10 @@
-// Power Set Algorithm
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         
-        for (int i = 0; i < (1 << nums.length); i++) {
+        int count = 1 << nums.length;
+        
+        for (int i = 0; i < count; i++) {
             List<Integer> vec = new ArrayList<>();
             for (int j = 0; j < nums.length; j++) {
                 if ((i & (1 << j)) != 0) {
