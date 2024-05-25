2699507ecbb5eaa44fa0636cb0bdb6d4e9fff1a8
Akash <80875594+akashzsh@users.noreply.github.com>
2024-05-25 10:08:55 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (87.09%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void f(int i, string& s, unordered_set<string>& set, string cur, vector<string>& res) {
-        if (i == s.size()) {
+    void f(int i, unordered_set<string>& dict, vector<string>& res, string cur, int& n, string& s) {
+        if (i == n) {
             cur.pop_back();
             res.push_back(cur);
             return;
         }
         
-        for (int j = i; j < s.size(); j++) {
-            string st = s.substr(i, j - i + 1);
-            if (set.find(st) != set.end()) {
-                f(j + 1, s, set, cur + st + " ", res);
+        for (int j = i; j < n; j++) {
+            string ck = s.substr(i, j - i + 1);
+            if (dict.find(ck) != dict.end()) {
+                f(j + 1, dict, res, cur + ck + " ", n, s);
             }
         }
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> set(wordDict.begin(), wordDict.end());
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
         
         vector<string> res;
         
-        f(0, s, set, "", res);
+        int n = s.size();
+        
+        f(0, dict, res, "", n, s);
         
         return res;
     }
