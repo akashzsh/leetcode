9da4800b042e15cec0e23104f26a9a5e88149e54
Akash <80875594+akashzsh@users.noreply.github.com>
2024-05-26 16:21:19 +0530

    Time: 4 ms (55.76%), Space: 11.7 MB (15.34%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,3 +1,26 @@
+// Recursion
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        ListNode* newHead = reverseList(head->next);
+        
+        ListNode *front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        
+        return newHead;
+    }
+};
+
+/*
+
+Iterative
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -14,3 +37,5 @@ public:
         return prev;
     }
 };
+
+*/
