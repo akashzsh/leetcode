5eb6416188faff0f570da419faa54d0afcaa054d
Akash <80875594+akashzsh@users.noreply.github.com>
2024-05-26 13:25:25 +0530

    Time: 950 ms (44.10%), Space: 386.5 MB (47.36%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -5,6 +5,42 @@
 */
 
 class Solution {
+public:    
+    int checkRecord(int n) {
+        int M = int(1e9) + 7;
+        
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3)));
+        
+        // base case
+        for (int ab = 0; ab < 2; ab++) {
+            for (int cl = 0; cl < 3; cl++) {
+                dp[0][ab][cl] = 1;
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int ab = 1; ab >= 0; ab--) {
+                for (int cl = 2; cl >= 0; cl--) {
+                    int res = 0;
+                    res = (res % M + dp[i - 1][ab][0] % M) % M;
+                    if (ab + 1 < 2) {
+                        res = (res % M + dp[i - 1][ab + 1][0] % M) % M;
+                    }
+                    if (cl + 1 < 3) {
+                        res = (res % M + dp[i - 1][ab][cl + 1] % M) % M;
+                    }
+
+                    dp[i][ab][cl] = res % M;
+                }
+            }
+        }
+        
+        return dp[n][0][0];
+    }
+};
+
+/*
+class Solution {
 public:
     int f(int n, int ab, int cl, int& M, vector<vector<vector<int>>>& dp) {
         if (ab > 1 || cl > 2) {
@@ -39,6 +75,7 @@ public:
         return f(n, 0, 0, M, dp);
     }
 };
+*/
 
 /*
 
