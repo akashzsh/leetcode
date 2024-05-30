2ad2630e618ead83ddd7861ddc334c93f8995ece
Akash <80875594+akashzsh@users.noreply.github.com>
2024-05-31 01:04:54 +0530

    Time: 508 ms (71.58%), Space: 124.3 MB (72.85%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -2,35 +2,38 @@ class Solution {
 public:
     typedef long long ll;
     
-    void f(int i, string ds, vector<string>& res, ll sum, ll prev, string& s, int& target) {
-        if (i == s.size()) {
+    void f(int i, string ds, ll sum, ll prev, vector<string>& res, string& s, int& target, int& N) {
+        if (i == N) {
             if (sum == target) {
                 res.push_back(ds);
             }
             return;
         }
+        
         ll number = 0;
         string acc = "";
-        for (int j = i; j < s.size(); j++) {
+        for (int j = i; j < N; j++) {
             if (j > i && s[i] == '0') {
                 break;
             }
-            acc = acc + s[j];
             number = number * 10 + (s[j] - '0');
+            acc = acc + s[j];
             if (i == 0) {
-                f(j + 1, ds + acc, res, number, number, s, target);
+                f(j + 1, ds + acc, number, number, res, s, target, N);
             } else {
-                f(j + 1, ds + "+" + acc, res, sum + number, number, s, target);
-                f(j + 1, ds + "-" + acc, res, sum - number, -number, s, target);
-                f(j + 1, ds + "*" + acc, res, (sum - prev) + (number * prev), number * prev, s, target);
+                f(j + 1, ds + "+" + acc, sum + number, number, res, s, target, N);
+                f(j + 1, ds + "-" + acc, sum - number, -number, res, s, target, N);
+                f(j + 1, ds + "*" + acc, (sum - prev) + (number * prev), prev * number, res, s, target, N);
             }
         }
     }
     
     vector<string> addOperators(string num, int target) {
+        int N = num.size();
+        
         vector<string> res;
         
-        f(0, "", res, 0, 0, num, target);
+        f(0, "", 0, 0, res, num, target, N);
         
         return res;
     }
