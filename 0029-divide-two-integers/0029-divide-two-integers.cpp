9c86697717a20e598d7d55d62d0a3dcdb83270cc
Akash <80875594+akashzsh@users.noreply.github.com>
2024-05-31 22:43:39 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (16.68%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
+    typedef long long ll;
+    
     int divide(int dividend, int divisor) {
-        if (dividend == divisor) {
-            return 1;
+        int sign = 1;
+        
+        if ((dividend >= 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+            sign = -1;
         }
         
-        bool is_neg = false;
+        ll num = abs(ll(dividend));
+        ll den = abs(ll(divisor));
+        // cout << "num is " << num << " and den is " << den << "\n";
+        ll res = 0;
         
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
-            is_neg = true;
-        }
-        
-        long long n = abs(dividend), d = abs(divisor);
-        
-        long long res = 0;
-        
-        while (n >= d) {
-            
-            int cnt = 0;
-            
-            while (n >= (d << (cnt + 1))) {
-                cnt++;
+        while (num >= den) {
+            int power = 0;
+            while (num >= (den << power)) {
+                power++;
             }
-            
-            res += (1 << cnt);
-            
-            n = n - (d << cnt);
+            power--;
+            num = num - (den << power);
+            res = res + (1 << power);
         }
         
         if (res == (1 << 31)) {
-            return is_neg ? INT_MIN : INT_MAX;
+            return sign == 1 ? INT_MAX : INT_MIN;
         }
         
-        return is_neg ? (int)(-res) : (int)(res);
+        return sign * res;
     }
 };
