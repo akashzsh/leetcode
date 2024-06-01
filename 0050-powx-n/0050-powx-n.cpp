52c54161a1bee7e2deb8607e7ff9c0151cb93e25
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-01 15:24:57 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (73.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    typedef long long ll;
-    
-    double f(double x, ll n) {
-        if (n == 0) {
-            return 1;
-        }
-        
-        if (n & 1) {
-            return x * f(x, n - 1);
-        } else {
-            return f(x * x, n / 2);
-        }
-    }
-    
     double myPow(double x, int n) {
-        int sign = 1;
-        ll m = ll(n);
+        bool is_positive = true;
+        double res = 1;
+        
+        long long N = n;
         
         if (n < 0) {
-            sign = -1;
-            m = m * -1;
+            is_positive = false;
+            N = -N;
         }
         
-        if (sign == 1) {
-            return f(x, m);
+        while (N > 0) {
+            if (N & 1) {
+                res = res * x;
+                N--;
+            } else {
+                x = x * x;
+                N >>= 1;
+            }
+        }
+        
+        if (is_positive) {
+            return res;
         } else {
-            return 1 / f(x, m);
+            return 1 / res;
         }
     }
 };
