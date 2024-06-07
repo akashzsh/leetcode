381497e8c275e576e7b55ac404db2a81df0951ca
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 20:42:19 +0530

    Time: 10 ms (59.05%), Space: 22.2 MB (83.05%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,26 @@
-// not optimal-- but for practising prefix sum
-
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> rg(n), lg(n);
+        int l = 0, r = height.size() - 1, lm = INT_MIN, rm = INT_MIN, res = 0;
         
-        lg[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            lg[i] = max(height[i], lg[i - 1]);
-        }
-        
-        rg[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rg[i] = max(height[i], rg[i + 1]);
-        }
-        
-        int water = 0;
-        
-        // for (const auto& it : rg) cout << it << " ";
-        // cout << "\n";
-        // for (const auto& it : lg) cout << it << " ";
-        // cout << "\n";
-        
-        for (int i = 0; i < n; i++) {
-            if (rg[i] == 0 || lg[i] == 0) {
-                continue;
+        while (l <= r) {
+            if (height[l] <= height[r]) {
+                if (height[l] > lm) {
+                    lm = height[l];
+                } else {
+                    res = res + (lm - height[l]);
+                }
+                l++;
+            } else {
+                if (height[r] > rm) {
+                    rm = height[r];
+                } else {
+                    res = res + (rm - height[r]);
+                }
+                r--;
             }
-            // cout << "adding " << min(rg[i], lg[i]) - height[i] << "\n";
-            water += min(rg[i], lg[i]) - height[i];
         }
         
-        return water;
+        return res;
     }
 };
