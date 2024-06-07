7a39bc6d59702c246503a0d13ae1b2aa2a281b36
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 18:35:13 +0530

    Time: 374 ms (77.42%), Space: 179.7 MB (94.87%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -26,22 +26,22 @@ public:
         tail = new Node(0, 0);
         head->next = tail;
         tail->prev = head;
-        length = 0;
+        this->length = 0;
     }
     
     void add(Node* node) {
-        length = length + 1;
         Node* head_next = head->next;
         head->next = node;
         node->prev = head;
-        head_next->prev = node;
         node->next = head_next;
+        head_next->prev = node;
+        length++;
     }
     
     void remove(Node* node) {
-        length = length - 1;
         node->prev->next = node->next;
         node->next->prev = node->prev;
+        length--;
     }
 };
 
@@ -49,33 +49,31 @@ class LFUCache {
 public:
     unordered_map<int, DLL*> fwm;
     unordered_map<int, Node*> xt;
-    int capacity;
-    int nodes;
-    int min_freq;
+    int min_freq, nodes, capacity;
     
     LFUCache(int capacity) {
         this->capacity = capacity;
         this->nodes = 0;
-        this->min_freq = 0;
+        this->min_freq = 1;
     }
     
     int get(int key) {
         if (xt.find(key) == xt.end()) {
             return -1;
         }
-        Node* node = xt[key];
-        update(node);
-        return node->val;
+        Node* cur = xt[key];
+        update(cur);
+        return cur->val;
     }
     
     void put(int key, int value) {
         if (xt.find(key) != xt.end()) {
-            Node* node = xt[key];
-            node->val = value;
-            update(node);
+            Node* cur = xt[key];
+            cur->val = value;
+            update(cur);
         } else {
+            DLL* list = nullptr;
             nodes++;
-            DLL* list;
             if (nodes > capacity) {
                 list = fwm[min_freq];
                 xt.erase(list->tail->prev->key);
@@ -83,15 +81,16 @@ public:
                 nodes--;
             }
             min_freq = 1;
-            if (fwm.find(min_freq) != fwm.end()) {
-                list = fwm[min_freq];
-            } else {
+            Node* new_node = new Node(key, value);
+            if (fwm.find(1) == fwm.end()) {
                 list = new DLL();
+            } else {
+                list = fwm[1];
             }
-            Node* node = new Node(key, value);
-            list->add(node);
-            fwm[min_freq] = list;
-            xt[key] = node;
+            
+            list->add(new_node);
+            fwm[1] = list;
+            xt[key] = new_node;
         }
     }
     
@@ -100,15 +99,18 @@ public:
         DLL* list = fwm[frequency];
         list->remove(node);
         fwm[frequency] = list;
+        
         if (frequency == min_freq && list->length == 0) {
-            min_freq += 1;
+            min_freq++;
         }
-        node->freq += 1;
-        if (fwm.find(node->freq) != fwm.end()) {
-            list = fwm[node->freq];
-        } else {
+        
+        node->freq++;
+        if (fwm.find(node->freq) == fwm.end()) {
             list = new DLL();
+        } else {
+            list = fwm[node->freq];
         }
+        
         list->add(node);
         fwm[node->freq] = list;
     }
