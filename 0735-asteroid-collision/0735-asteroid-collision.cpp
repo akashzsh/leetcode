3241294e6c7127f129bcdbea0c690fd4aba28e77
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 22:33:33 +0530

    Time: 10 ms (56.20%), Space: 20.6 MB (52.45%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,23 +3,22 @@ public:
     vector<int> asteroidCollision(vector<int>& ast) {
         stack<int> st;
         
-        for (int& cur : ast) {
-            while (!st.empty() && cur < 0 && st.top() > 0) {
+        for (auto& ball : ast) {
+            while (!st.empty() && ball < 0 && st.top() > 0) {
+                int sum = ball + st.top();
                 
-                int sum = cur + st.top();
-                
-                if (sum < 0) {
+                if (sum > 0) {
+                    ball = 0;
+                } else if (sum < 0) {
                     st.pop();
-                } else if (sum > 0) {
-                    cur = 0;
                 } else {
                     st.pop();
-                    cur = 0;
+                    ball = 0;
                 }
             }
             
-            if (cur != 0) {
-                st.push(cur);
+            if (ball != 0) {
+                st.push(ball);
             }
         }
         
