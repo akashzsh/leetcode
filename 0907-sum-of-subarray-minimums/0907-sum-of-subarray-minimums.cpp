2484ee37d49ee0e9f2087eceb0b74888f01c1e2c
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 21:26:33 +0530

    Time: 64 ms (61.31%), Space: 41.8 MB (94.17%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -3,19 +3,16 @@ public:
     typedef long long ll;
     
     int sumSubarrayMins(vector<int>& arr) {
-        constexpr int M = int(1e9) + 7;
-        
+        int M = int(1e9) + 7;
         stack<int> st;
-        ll res = 0;
-        int n = arr.size();
         
-        for (int k = 0; k <= n; k++) {
+        ll res = 0;
+        
+        for (int k = 0, n = arr.size(); k <= n; k++) {
             while (!st.empty() && (k == n || arr[st.top()] > arr[k])) {
-                int ls;
                 int j = st.top(); st.pop();
                 int i = st.empty() ? -1 : st.top();
-                
-                res = (res % M + (ll(arr[j]) * (k - j) * (j - i)) % M) % M;
+                res = ((res % M) + ll(arr[j]) * (k - j) * (j - i) % M) % M;
             }
             st.push(k);
         }
