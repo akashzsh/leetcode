2cf24d01159bd427e3cb4e5105d4827118b90147
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 12:28:54 +0530

    Time: 27 ms (40.50%), Space: 32.3 MB (35.54%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -5,13 +5,23 @@ public:
             return 0;
         }
         
+        unordered_map<int, int> mp;
         int cnt = 0;
         
-        for (int l = 0, r = 0, n = nums.size(), sum = 0; r < n; r++) {
-            sum += nums[r];
+        for (int l = 0, r = 0, n = nums.size(), ones = 0; r < n; r++) {
+            if (nums[r] == 1) {
+                ones++;
+            }
+            mp[nums[r]]++;
             
-            while (sum > k) {
-                sum -= nums[l];
+            while (ones > k) {
+                if (nums[l] == 1) {
+                    ones--;
+                }
+                mp[nums[l]]--;
+                if (mp[nums[l]] == 0) {
+                    mp.erase(nums[l]);
+                }
                 l++;
             }
             
