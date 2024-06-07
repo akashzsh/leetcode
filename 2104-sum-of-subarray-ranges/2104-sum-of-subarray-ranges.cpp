b00084ea3437b4d79cefc77eaa85d7f8dddf5e13
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-07 22:49:59 +0530

    Time: 3 ms (99.73%), Space: 13 MB (57.45%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -3,27 +3,25 @@ public:
     typedef long long ll;
     
     long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-            
         ll res = 0;
+        stack<int> st;
         
-        for (int k = n - 1; k >= -1; k--) {
-            while (!st.empty() && (k == -1 || nums[st.top()] < nums[k])) {
-                int j = st.top(); st.pop();
-                int i = st.empty() ? n : st.top();
-                res += ll(nums[j]) * (i - j) * (j - k);
-            }
-            st.push(k);
-        }
-        
-        st = stack<int>();
-        
-        for (int k = 0; k <= n; k++) {
+        for (int k = 0, n = nums.size(); k <= n; k++) {
             while (!st.empty() && (k == n || nums[st.top()] > nums[k])) {
                 int j = st.top(); st.pop();
                 int i = st.empty() ? -1 : st.top();
                 res -= ll(nums[j]) * (k - j) * (j - i);
+            }
+            st.push(k);
+        }
+        
+        while (!st.empty()) st.pop();
+        
+        for (int k = 0, n = nums.size(); k <= n; k++) {
+            while (!st.empty() && (k == n || nums[st.top()] < nums[k])) {
+                int j = st.top(); st.pop();
+                int i = st.empty() ? -1 : st.top();
+                res += ll(nums[j]) * (k - j) * (j - i);
             }
             st.push(k);
         }
