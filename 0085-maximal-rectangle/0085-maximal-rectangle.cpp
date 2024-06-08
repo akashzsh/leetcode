8b188c47411084ef4510e2628bec7d272c609646
Akash <80875594+akashzsh@users.noreply.github.com>
2024-06-08 18:03:30 +0530

    Time: 29 ms (69.94%), Space: 17.4 MB (50.45%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int largest_rectangle_in_histogram(vector<int>& nums) {
-        stack<int> st;
         int res = 0;
+        stack<int> st;
+        
+        // for (auto& it : nums) cout << it << " ";
+        // cout << "\n";
         
         for (int i = 0, n = nums.size(); i <= n; i++) {
             while (!st.empty() && (i == n || nums[st.top()] >= nums[i])) {
-                int height = nums[st.top()]; st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                res = max(res, height * width);
+                int h = nums[st.top()]; st.pop();
+                int w = st.empty() ? i : i - st.top() - 1;
+                res = max(res, h * w);
             }
             st.push(i);
         }
@@ -16,17 +19,15 @@ public:
         return res;
     }
     
-    int maximalRectangle(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> row(n);
-        
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        vector<int> nums(matrix[0].size());
         int res = 0;
         
-        for (int i = 0; i < m; i++) {
+        for (int i = 0, m = matrix.size(), n = matrix[0].size(); i < m; i++) {
             for (int j = 0; j < n; j++) {
-                row[j] = (grid[i][j] == '0') ? 0 : (row[j]) + (grid[i][j] - '0');
+                nums[j] = matrix[i][j] == '0' ? 0 : nums[j] + 1;
             }
-            res = max(res, largest_rectangle_in_histogram(row));
+            res = max(res, largest_rectangle_in_histogram(nums));
         }
         
         return res;
